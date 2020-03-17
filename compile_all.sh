d65dcf100d9bf7522bc9793bc592f7db02c1e9c3
tomztyang <33257287+tomztyang@users.noreply.github.com>
2020-03-17 14:31:16 +0800

    environment files and configs

---
+++ compile_all.sh
@@ -0,0 +1,42 @@
+# evaluation 
+TFPATH=$1 # e.g: /usr/local/lib/python3.5/dist-packages/tensorflow
+CUDAPATH=$2 # e.g: /usr/local/cuda-9.0
+OPSPATH="lib/utils/tf_ops"
+
+# install dependencies
+pip install -r requirements.txt
+
+# evaluation
+cd ${OPSPATH}/evaluation
+g++ -std=c++11 tf_evaluate.cpp -o tf_evaluate_so.so -shared -fPIC -I ${TFPATH}/include -I ${CUDAPATH}/include -I ${TFPATH}/include/external/nsync/public -lcudart -L ${CUDAPATH}/lib64/ -L${TFPATH} -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+cd ..
+
+# grouping
+cd grouping
+$CUDAPATH/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I $TFPATH/include -I $CUDAPATH/include -I $TFPATH/include/external/nsync/public -lcudart -L $CUDAPATH/lib64/ -L$TFPATH -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+cd ..
+
+# interpolation
+cd interpolation
+$CUDAPATH/bin/nvcc tf_interpolate_g.cu -o tf_interpolate_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TFPATH/include -I $CUDAPATH/include -I $TFPATH/include/external/nsync/public -lcudart -L $CUDAPATH/lib64/ -L$TFPATH -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+cd ..
+
+# points pooling
+cd points_pooling
+$CUDAPATH/bin/nvcc tf_points_pooling_g.cu -o tf_points_pooling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+g++ -std=c++11 tf_points_pooling.cpp tf_points_pooling_g.cu.o -o tf_points_pooling_so.so -shared -fPIC -I $TFPATH/include -I $CUDAPATH/include -I $TFPATH/include/external/nsync/public -lcudart -L $CUDAPATH/lib64/ -L$TFPATH -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+cd ..
+
+# sampling
+cd sampling
+$CUDAPATH/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TFPATH/include -I $CUDAPATH/include -I $TFPATH/include/external/nsync/public -lcudart -L $CUDAPATH/lib64/ -L$TFPATH -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+cd ..
+
+# nms
+cd nms
+./build.sh
+cd ..
+
