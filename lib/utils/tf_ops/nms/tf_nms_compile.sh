06bc693f0acf506ba049da08b8520e34018e8d07
nowsyn <nowsyn@126.com>
2020-04-08 16:50:36 +0800

    Remove useless files

--- lib/utils/tf_ops/nms/tf_nms_compile.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-/usr/local/cuda-9.0/bin/nvcc nms_kernel.cu -o nms_kernel.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
-# TF1.2
-#g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-
-# TF1.4
-g++ -std=c++11 tf_nms.cpp nms_kernel.cu.o -o tf_nms_so.so -shared -fPIC -I /usr/local/lib/python3.5/dist-packages/tensorflow/include -I /usr/local/cuda-9.0/include -I /usr/local/lib/python3.5/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-9.0/lib64/ -L/usr/local/lib/python3.5/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
--- lib/utils/tf_ops/sampling/tf_sampling_compile.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-/usr/local/cuda-9.0/bin/nvcc -ccbin /usr/bin/gcc-5 tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
-# TF1.4
-/usr/bin/gcc-5 -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I ~/anaconda3/envs/pc_detector/lib/python3.6/site-packages/tensorflow/include -I /usr/local/cuda-9.0/include -I ~/anaconda3/envs/pc_detector/lib/python3.6/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-9.0/lib64/ -L ~/anaconda3/envs/pc_detector/lib/python3.6/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
